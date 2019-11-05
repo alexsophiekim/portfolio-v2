5a4cfae3592e4cb642545e43bb3f4c11f5211226
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-11-05 20:36:15 +1300

    edited content

--- js/script.js
+++ js/script.js
@@ -19,21 +19,3 @@ $(document).ready(function(){
 function scrollDown() {
   window.scrollTo(0, 680);
 }
-
-var textWrapper = document.querySelector('.ml16');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-
-anime.timeline({loop: false})
-  .add({
-    targets: '.ml16 .letter',
-    translateY: [-100,0],
-    // easing: "easeOutExpo",
-    duration: 1000,
-    delay: (el, i) => 30 * i
-  }).add({
-    targets: '.ml16',
-    opacity: 0.8,
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 1000
-  });
