19d187f396d1e33571de7e9fc6c8a082b8d333f5
Sophie <48259684+alexsophiekim@users.noreply.github.com>
2019-11-11 22:11:27 +1300

    want to add animation javascriptscroll based

--- js/script.js
+++ js/script.js
@@ -18,4 +18,25 @@ $(document).ready(function(){
 
 function scrollDown() {
   window.scrollTo(0, 680);
-}
+};
+
+
+const about = $('.about');
+const skills = $('.skills');
+
+about.click(function(){
+  console.log('clicked');
+    $('.aboutText').addClass('animation').toggle();
+});
+  //
+  // var skillSet = $('.skillSet');
+  // var offset = 300;
+  // var skillSetOffsetTop = skillSet.offset().top - offset;
+  // $(window).scroll(function(){
+  //   if ($(this).scrollTop() > skillSetOffsetTop) {
+  //     skillSet.find('graph').addClass('animateLine');
+  //   }
+  //   else {
+  //
+  //   }
+  // });
